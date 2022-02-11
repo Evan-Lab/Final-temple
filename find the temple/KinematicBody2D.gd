f497f6df6e752b003cd4662f68999c89eae1c427
parosa47 <91087118+parosa47@users.noreply.github.com>
2022-02-11 20:43:56 +0100

    1

---
+++ find the temple/KinematicBody2D.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+var speed = 400
+
+var velocity = Vector2()
+
+func _physics_process(delta):
+	velocity.x = (int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))) * speed
+	velocity.y = (int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))) * speed
+	$AnimatedSprite.play()
+	if (velocity.x > 0):
+		$AnimatedSprite.animation = "EST"
+	if (velocity.y > 0):
+		$AnimatedSprite.animation = "SOUTH"
+	if (velocity.x < 0):
+		$AnimatedSprite.animation = "WEST"
+	if (velocity.y < 0):
+		$AnimatedSprite.animation = "NORTH"
+	if	(velocity.x == 0 and velocity.y == 0):
+		$AnimatedSprite.stop()
+
+	
+	move_and_slide(velocity, Vector2(0, -1))
