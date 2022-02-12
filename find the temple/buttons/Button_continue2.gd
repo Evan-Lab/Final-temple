07db8a17e389ba3ac326e12dfd13139c9ecfc6b9
leo lhuillier <91798341+Kaloneru@users.noreply.github.com>
2022-02-12 19:52:29 +0100

    ok

---
+++ find the temple/buttons/Button_continue2.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_Button_continue2_pressed():
+	get_tree().change_scene("res://lore3.tscn")
---
+++ find the temple/buttons/Button_continue3.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_Button_continue3_pressed():
+	get_tree().change_scene("res://BL_lore4.tscn")
