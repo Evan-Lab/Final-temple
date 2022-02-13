136b574534fdb66f1a561392f114b8c9fe1d27f5
leo lhuillier <91798341+Kaloneru@users.noreply.github.com>
2022-02-13 15:06:49 +0100

    ok

---
+++ find the temple/Button_AL_continue3.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_Button_AL_continue3_pressed():
+	get_tree().change_scene("res://AL_lore4.tscn")
---
+++ find the temple/buttons/Button_AL_continue1.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_Button_AL_continue1_pressed():
+	get_tree().change_scene("res://AL_lore2.tscn")
