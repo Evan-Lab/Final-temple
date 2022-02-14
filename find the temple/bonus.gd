b0b50b24ef5f7af10482b92504302457a0f89633
parosa47 <91087118+parosa47@users.noreply.github.com>
2022-02-14 19:47:47 +0100

    ok

--- find the temple/bonus.gd
+++ find the temple/bonus.gd
@@ -4,29 +4,34 @@ func _ready():
 	pass 
 
 
+
 func _on_WALL1_body_entered(body):
 	$AudioStreamPlayer.play()
 	$"lab-KinematicBody2D/Camera2D".zoom.x = $"lab-KinematicBody2D/Camera2D".zoom.x + 0.3
 	$"lab-KinematicBody2D/Camera2D".zoom.y = $"lab-KinematicBody2D/Camera2D".zoom.y + 0.3
+	$AudioStreamPlayer.stop()
 	pass # Replace with function body.
 
 
 func _on_WALL2_body_entered(body):
+	$AudioStreamPlayer.play()
 	$"lab-KinematicBody2D/Camera2D".zoom.x = $"lab-KinematicBody2D/Camera2D".zoom.x + 0.3
 	$"lab-KinematicBody2D/Camera2D".zoom.y = $"lab-KinematicBody2D/Camera2D".zoom.y + 0.3
-	$AudioStreamPlayer.play()
+	$AudioStreamPlayer.stop()
 	pass # Replace with function body.
 
 
 func _on_WALL3_body_entered(body):
+	$AudioStreamPlayer.play()
 	$"lab-KinematicBody2D/Camera2D".zoom.x = $"lab-KinematicBody2D/Camera2D".zoom.x + 0.3
 	$"lab-KinematicBody2D/Camera2D".zoom.y = $"lab-KinematicBody2D/Camera2D".zoom.y + 0.3
-	$AudioStreamPlayer.play()
+	$AudioStreamPlayer.stop()
 	pass # Replace with function body.
 
 
 func _on_WALL4_body_entered(body):
+	$AudioStreamPlayer.play()
 	$"lab-KinematicBody2D/Camera2D".zoom.x = $"lab-KinematicBody2D/Camera2D".zoom.x + 0.3
 	$"lab-KinematicBody2D/Camera2D".zoom.y = $"lab-KinematicBody2D/Camera2D".zoom.y + 0.3
-	$AudioStreamPlayer.play()
+	$AudioStreamPlayer.stop()
 	pass # Replace with function body.
